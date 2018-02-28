6569ec52f99a5da8c3f5dcef10a944db7fa4c290
Jaime <jaimebmorin@gmail.com>
2018-02-27 23:35:14 -0600

    twitter login

---
+++ config/auth.js
@@ -0,0 +1,9 @@
+// epose our config directly to our application using module.exports
+
+module.exports = {
+    'twitterAuth' : {
+        'consumerKey' : 'LFUZVdBttwq4DQwu0NNqTeoMI',
+        'consumerSecret' : '4RtyFsyofk8jP6UsKJhZwSKNaMohO89hgDwbkfTWkmpANHNNOV',
+        'callbackURL' : 'http://127.0.0.1:8080/auth/twitter/callback'
+    }
+};
